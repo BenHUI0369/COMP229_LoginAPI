de62cf17477a587ad270220de86dbb9ae6ae6021
SIU WAI HUI <huisiuwaiben0303@gmail.com>
2023-07-06 03:18:44 -0400

    done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const logger = require("./logger");
 const routes = require('./routes');
 const connectToDatabase = require("./database");
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 
 app.use('/api', routes);
 
